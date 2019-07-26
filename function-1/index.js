9449ffa37152d492233f2fb89b0ffbe6264ad9bb
Ahmed Awadallah <aawadall@ualberta.ca>
2019-07-26 11:20:38 -0400

    bootstrap project

---
+++ function-1/index.js
@@ -0,0 +1,21 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+
+const { checkSchema } = require('express-validator');
+
+exports.helloWorld = (req, res) => {
+  const method = req.method;
+  let message = req.query.message || req.body.message || 'Empty Request';
+  let reply = {
+  	path : req.path,
+    query : req.query,
+    body : req.body,
+    method: req.method
+  };
+  
+  res.status(200).send(reply);
+};
